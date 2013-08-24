55dbf7425d0ce66f1d10d2cb959c31f86be41413
David W <davidw@danvil.de>
2013-08-24 21:11:10 +0200

    Added level transitions

--- Assets/Scripts/Announcer.cs
+++ Assets/Scripts/Announcer.cs
@@ -35,24 +35,69 @@ public class Announcer : MonoBehaviour {
 			}
 			if(Globals.Player.IsDead) {
 				Globals.HasEnded = true;
+				dt = Globals.BRAVO_TIME;
 			}
 			if(Globals.Clock.TimeOut) {
 				Globals.HasEnded = true;
+				dt = Globals.BRAVO_TIME;
 			}
 			if(Globals.ExitReached) {
 				Globals.HasEnded = true;
+				dt = Globals.BRAVO_TIME;
 			}
 		}
 		if(Globals.HasEnded) {
 			MyTime.Pause = true;
-			if(Globals.Player.IsDead) {
-				guiText.text = "... (argh!) ...";
+			if(!Globals.ExitReached) {
+				dt -= Time.deltaTime;
+				if(dt > 0) {
+					if(Globals.Player.IsDead) {
+						guiText.text = "... (argh!) ...";
+					}
+					if(Globals.Clock.TimeOut) {
+						guiText.text = "Time over ...";
+					}
+					if(Input.GetButtonDown("Jump")) {
+						dt = 0.0f;
+					}
+				}
+				else {
+					guiText.text = "Press JUMP to retry the level";
+					if(Input.GetButtonDown("Jump")) {
+						// repeat level
+						string lvl = string.Format("level_{0:000}", Globals.Level);
+						Globals.HasStarted = false;
+						Globals.HasEnded = false;
+						Globals.ExitReached = false;
+						Application.LoadLevel(lvl); 
+					}
+				}
 			}
-			if(Globals.Clock.TimeOut) {
-				guiText.text = "Time over ...";
-			}
-			if(Globals.ExitReached) {
-				guiText.text = "Bravo!";
+			else {
+				if(Globals.Level == Globals.LastLevel) {
+					guiText.text = "Yeah!\nYou completed all " + Globals.Level.ToString() + " levels!";
+				}
+				else {
+					dt -= Time.deltaTime;
+					if(dt > 0) {
+						guiText.text = "Bravo!";
+						if(Input.GetButtonDown("Jump")) {
+							dt = 0.0f;
+						}
+					}
+					else {
+						guiText.text = "Press JUMP for next level";
+						if(Input.GetButtonDown("Jump")) {
+							// next level
+							Globals.Level ++;
+							string lvl = string.Format("level_{0:000}", Globals.Level);
+							Globals.HasStarted = false;
+							Globals.HasEnded = false;
+							Globals.ExitReached = false;
+							Application.LoadLevel(lvl); 
+						}
+					}
+				}
 			}
 		}
 	}
