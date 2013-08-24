011a24740fb28b056a9550fe3cf7f26c9ca14f7f
David W <davidw@danvil.de>
2013-08-24 12:39:10 +0200

    Added walls to level.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,19 +6,28 @@ public class Block : MonoBehaviour {
 	public float wobbleDuration = 10.0f;
 	public float wobbleAplitude = 0.05f;
 	
-	public float baseHeight;
-	public float basePhase;
+	public Vector3 basePosition;
+
+	public Vector3 offset;
+
+	public float phaseOffset;
 	
 	// Use this for initialization
 	void Start () {
-		baseHeight = -0.05f + 0.1f*Random.value;
-		basePhase = Random.value;
+		offset = new Vector3(
+			-0.05f + 0.1f*Random.value,
+			-0.05f + 0.1f*Random.value,
+			-0.05f + 0.1f*Random.value);
+		phaseOffset = Random.value;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		float h = Mathf.Sin((MyTime.time / wobbleDuration + basePhase) * (2.0f * Mathf.PI));
-		this.transform.localPosition = this.transform.localPosition.ChangeY(baseHeight + wobbleAplitude*h);
+		float h = Mathf.Sin((MyTime.time / wobbleDuration + phaseOffset) * (2.0f * Mathf.PI));
+		this.transform.localPosition =
+			basePosition
+			+ offset
+			+ new Vector3(0, wobbleAplitude*h, 0);
 	}
 }
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -4,16 +4,51 @@ using System.Collections;
 public class World : MonoBehaviour
 {
 	public GameObject blockBase;
-
+	
+	void AddBlock(float x, float y, float z)
+	{
+ 		GameObject go = (GameObject)Instantiate(blockBase);
+ 		Block block = go.GetComponent<Block>();
+ 		block.basePosition = new Vector3(x,y,z);
+		go.transform.parent = this.transform;
+	}
+	
 	void CreateAWorld()
 	{
+		int W = 12;
+		int D = 5;
+		int H = 8;
+		int WO = 5;
 		// floor
-		for(int x=0; x<10; x++) {
-			for(int z=0; z<4; z++) {
-		 		GameObject go = (GameObject)Instantiate(blockBase);
-				go.transform.parent = this.transform;
-				go.transform.localPosition = new Vector3(x,0,z);
+		for(int x=0; x<W; x++) {
+			for(int z=0; z<D; z++) {
+				AddBlock(x, 0, z);
 		 	}
+		}
+		// back wall
+		for(int x=0; x<W; x++) {
+			for(int h=0; h<H; h++) {
+				AddBlock(x, h, D);
+			}
+		}
+		// left wall
+		for(int z=0; z<=D; z++) {
+			for(int h=0; h<H; h++) {
+				AddBlock(-1, h, z);
+			}
+		}
+		// right wall
+		for(int z=0; z<=D; z++) {
+			for(int h=0; h<H; h++) {
+				AddBlock(W, h, z);
+			}
+		}
+		// front wall
+		for(int x=-WO; x<=W+WO; x++) {
+			for(int h=0; h<H; h++) {
+				if(x < -1 || W < x)
+					AddBlock(x, h, 0);
+			}
 		}
 	}
 
